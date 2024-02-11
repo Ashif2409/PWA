b02e0443ea5aebbf898fa94490c48cdcb09ec8fd
Md Khalilul Rahman <105421454+Ashif2409@users.noreply.github.com>
2024-02-11 10:11:23 +0530

    Update home.js

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -9,42 +9,8 @@ const Home = ({ data }) => {
   const [msg, setMsg] = useState('');
   const [inputValue, setInputValue] = useState('');
   const msgContainerRef = useRef(null);
-  const [file, setFile] = useState();
-  const [uploadedFile, setUploadedFile] = useState();
   const [error, setError] = useState();
 
-  function handleChanges(event) {
-    setFile(event.target.files[0]);
-  }
-  const handleDownload = (file, fileName) => {
-    const link = document.createElement('a');
-    link.href = file;
-    link.download = fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  const createMessageContainer = (fileName) => {
-    const msgContainer = document.createElement('div');
-    msgContainer.classList.add('middle');
-  
-    const messageText = document.createElement('span');
-  
-    
-    const fileType = fileName.split('.').pop().toLowerCase();
-    if (fileType === 'mp3' || fileType === 'wav' || fileType === 'ogg') {
-      messageText.textContent = `Audio file ${fileName} has been shared.`;
-    } else if (fileType === 'mp4' || fileType === 'webm' || fileType === 'mov') {
-      messageText.textContent = `Video file ${fileName} has been shared.`;
-    } else {
-      messageText.textContent = `File ${fileName} has been shared.`;
-    }
-  
-    msgContainer.appendChild(messageText);
-  
-    return msgContainer;
-  };
 
   useEffect(() => {
     socket.emit('userJoin', data);
@@ -69,59 +35,7 @@ const Home = ({ data }) => {
   }, [data]);
 
   
-  useEffect(() => {
-    socket.on('receiveAudio', ({ file, fileName }) => {
-
-      const audio = document.createElement('audio');
-      audio.src = file;
-      audio.controls = true;
-      const msgContainer = createMessageContainer(fileName);
-      msgContainer.appendChild(audio);
-      msgContainerRef.current.appendChild(msgContainer);
-    });
-
-    // socket.on('receiveVideo', ({ file, fileName }) => {
-    //   const video = document.createElement('video');
-    //   video.src = file;
-    //   video.controls = true;
-
-    //   const msgContainer = createMessageContainer(fileName);
-    //   msgContainer.appendChild(video);
-    //   msgContainerRef.current.appendChild(msgContainer);
-    // });
-
-    socket.on('receiveFile', ({ file, fileName }) => {
-      const message = `${fileName} has been shared. Download: `;
-
-      // Create a download link
-      const downloadLink = document.createElement('a');
-      downloadLink.href = file;
-      downloadLink.target = '_blank'; 
-      downloadLink.rel = 'noopener noreferrer'; 
-      downloadLink.textContent = fileName;
-
-     
-      downloadLink.addEventListener('click', (event) => {
-        event.preventDefault();
-        handleDownload(file, fileName);
-      });
-
-      const msgContainer = document.createElement('div');
-      msgContainer.classList.add('middle');
-
-      msgContainer.appendChild(document.createTextNode(message));
-      msgContainer.appendChild(downloadLink);
-
-      msgContainerRef.current.appendChild(msgContainer);
-    });
-
-    return () => {
-      socket.off('receiveFile');
-      socket.off('receiveVideo');
-      socket.off('receiveAudio');
-    };
-  }, []);
-
+ 
 
   const submit = () => {
     const message = msg;
@@ -154,31 +68,6 @@ const Home = ({ data }) => {
   };
 
 
-  function handleSubmit(event) {
-
-    event.preventDefault();
-    const url = 'http://localhost:9002/uploadFile';
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', file.name);
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data',
-      },
-    };
-    axios.post(url, formData, config)
-      .then((response) => {
-        console.log(response.data);
-        setUploadedFile(response.data.file);
-        socket.emit('sendFile', { file: response.data.file, fileName: file.name });
-      })
-      .catch((error) => {
-        console.error("Error uploading file: ", error);
-        setError(error);
-      });
-  }
-
-
   return (
     <>
       <div className="container">
@@ -188,10 +77,6 @@ const Home = ({ data }) => {
           <button className="sendBtn" onClick={submit}>
             Send
           </button>
-          <form onSubmit={handleSubmit}>
-            <input type="file" onChange={handleChanges} />
-            <button type="submit">Upload</button>
-          </form>
         </div>
       </div>
     </>
